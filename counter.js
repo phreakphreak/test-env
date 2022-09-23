651d81b2c88fc05556ef133f19d54ef3366d68f5
Michael Zabala(Mike) <wonkaphreak@gmail.com>
2022-09-23 14:35:40 -0400

    agrego console.log

--- counter.js
+++ counter.js
@@ -1,5 +1,8 @@
 export function setupCounter(element) {
   let counter = 0
+
+  console.log("hello world")
+
   const setCounter = (count) => {
     counter = count
     element.innerHTML = `count is ${counter}`
